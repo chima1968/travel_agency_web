352a75e2254c57d2d5b308c70e721afacedd3b56
chima1968 <chimarokeumunna98@gmail.com>
2024-12-19 16:52:48 +0100

    nav_feature_update

---
+++ app.js
@@ -0,0 +1,14 @@
+const title = document.getElementById('headingTwo');
+
+console.log(title)
+
+
+const sect=document.getElementsByClassName('accordion-item')
+
+console.log(sect)
+
+const part=document.querySelectorAll('div');
+
+console.log(part)
+
+title.style.color='green'
